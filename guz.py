281458a15d31e8ca71f55c75869fb4b2728077df
E Pogrebnyak <e.pogrebnyak@gmail.com>
2017-08-21 13:59:31 +0300

    stable tests

--- guz.py
+++ guz.py
@@ -1,29 +1,29 @@
 """Organise tasks with due date, text file, project and context tags.
 
   new         Create new task description
-  list        List tasks 
-  <n> command Perform command on task 
+  list        List tasks
+  <n> command Perform command on task
 
 Usage:
   guz.py new <textlines>...
   guz.py list [<patterns>...]
-  guz.py del <n> 
+  guz.py del <n>
   guz.py <n> as <textlines>...
-  guz.py <n> mark (done  | -d) 
-  guz.py <n> mark (fail  | -f) 
-  guz.py <n> mark (doubt | -?) 
-  guz.py <n> mark (wait  | -w) [<input>] 
-  guz.py <n> unmark 
-  guz.py <n> due <datestamp> 
-  guz.py <n> file <filename> 
-  guz.py <n> [+<project>]... 
+  guz.py <n> mark (done  | -d)
+  guz.py <n> mark (fail  | -f)
+  guz.py <n> mark (doubt | -?)
+  guz.py <n> mark (wait  | -w) [<input>]
+  guz.py <n> unmark
+  guz.py <n> due <datestamp>
+  guz.py <n> file <filename>
+  guz.py <n> [+<project>]...
   guz.py <n> [@<context>]...
-  guz.py (rebase | delete) all   
+  guz.py (rebase | delete) all
   guz.py datafile [<path>]
-  guz.py [timer] start <n> 
+  guz.py [timer] start <n>
   guz.py [timer] stop
-  guz.py -h  
-  
+  guz.py -h
+
 Options:
   -h --help     Show this screen.
 """
@@ -42,41 +42,43 @@ FILENAME = 'data.pickle'
 
 
 class DataStore(object):
-    """Store data in a local file. 
-    
+    """Store data in a local file.
+
        Uses pickle with *self.filename* to store data.
-       
-    """    
+
+    """
+
     def __init__(self, filename=FILENAME):
         """
-        If *filename* does not exist, writes empty 
+        If *filename* does not exist, writes empty
         dictionary to *filename*.
         """
-        self.filename = filename            
+        self.filename = filename
         if not os.path.exists(self.filename):
             self.to_disk({})
-    
+
     def to_disk(self, x):
         with open(self.filename, 'wb') as fp:
             pickle.dump(x, fp)
 
     def from_disk(self):
         with open(self.filename, 'rb') as fp:
-            return pickle.load(fp)    
-    
+            return pickle.load(fp)
+
 
 class Status(object):
     # ENUM?
     allowed = {'none': ' ',
-               'done': '+', 
-               'wait': 'e', 
+               'done': '+',
+               'wait': 'e',
                'fail': 'f',
-               'doubt': '?'}    
+               'doubt': '?'}
+
 
 class Task(object):
-    
+
     def __init__(self, subject: str):
-        self.__dict__ = dict(subject=subject, status=None) 
+        self.__dict__ = dict(subject=subject, status=None)
 
     def __setstate__(self, x):
         """Function to work with pickle.load()"""
@@ -86,35 +88,37 @@ class Task(object):
         """Function to work with pickle.dump()"""
         return self.__dict__
 
-    def __getattr__(self, key): 
+    def __getattr__(self, key):
         """Return task attibutes by name.
-        
+
         Example:
             Task("go to holiday").subject
         """
         return self.__dict__[key]
 
-    def __repr__(self):  
+    def __eq__(self, x):
+        return bool(self.__dict__ == x.__dict__)
+
+    def __repr__(self):
         return "Task(subject='{}')".format(self.subject)
-       
-    def __str__(self):  
+
+    def __str__(self):
         return self.subject
-    
+
     def format_with_id(self, id):
         i = "{:2d}".format(id)  # see pyformat
-        status = " "            #{'done':'x', None:" "}[self.status]
-        return "{} [{}] {}".format(i, status, self)    
+        status = " "  # {'done':'x', None:" "}[self.status]
+        return "{} [{}] {}".format(i, status, self)
 
-        
 
-class TaskList:    
-    
-    def __init__(self, path=FILENAME, out = sys.stdout):
-        self.store = DataStore(path) 
+class TaskList:
+
+    def __init__(self, path=FILENAME, out=sys.stdout):
+        self.store = DataStore(path)
         self.tasks = self.store.from_disk()
         self.out = out
-    
-    @property   
+
+    @property
     def ids(self):
         return [x for x in self.tasks.keys() if isinstance(x, int)]
 
@@ -123,29 +127,30 @@ class TaskList:
 
     def accept_id(self, id):
         if id in self.ids:
-           return True
+            return True
         else:
-           self.echo("No such task id: {}".format(id))             
-           return False
-       
+            self.echo("No such task id: {}".format(id))
+            return False
+
     def rebase(self):
-        self.tasks = {(new_id+1):self.tasks[id] for new_id, id 
-                                                in enumerate(self.ids)}
+        self.tasks = {(new_id + 1): self.tasks[id]
+                      for new_id, id
+                      in enumerate(self.ids)}
         self.echo("Rebased task ids")
         self.save()
-            
+
     def delete_item(self, id, silent=False):
         if self.accept_id(id):
             del self.tasks[id]
             self.save()
             if not silent:
-                self.echo("Deleted task {}".format(id))            
+                self.echo("Deleted task {}".format(id))
         else:
             self.echo("Cannot delete task {}".format(id))
 
     def delete_all(self):
         for id in self.ids:
-           self.delete_item(id, silent=True)   
+            self.delete_item(id, silent=True)
         self.save()
         self.echo("All tasks deleted. What made you do this?..")
 
@@ -155,26 +160,26 @@ class TaskList:
         self.save()
         self.echo("New task added:")
         self.echo_task(id)
-        
+
     def replace_item(self, id, task):
         self.tasks[id] = task
         self.save()
         self.echo("Task changed:")
         self.echo_task(id)
-    
+
     def save(self):
-        self.store.to_disk(self.tasks) 
-    
+        self.store.to_disk(self.tasks)
+
     def echo(self, msg):
         print(msg, file=self.out)
 
-    def echo_task(self, id):       
+    def echo_task(self, id):
         task_repr = self.tasks[id].format_with_id(id)
         print(task_repr, file=self.out)
-   
-    def select_all(self):
+
+    def select_all_ids(self):
         return sorted(self.ids)
-    
+
     def select(self, patterns):
         def find_pattern(pat, text):
             if pat.startswith("-"):
@@ -186,44 +191,45 @@ class TaskList:
             flag = [True for pat in patterns if find_pattern(pat, text)]
             if flag:
                 yield id
-    
+
     def list(self, ids=False):
         if not ids:
-            ids = self.select_all()
+            ids = self.select_all_ids()
         for id in ids:
             self.echo_task(id)
-        msg = "Listed {} of {} tasks".format(len(ids), len(self.ids))    
+        msg = "Listed {} of {} tasks".format(len(ids), len(self.ids))
         self.echo(msg)
+
 
 class Arguments:
     """Convert command line arguments to variables using docopt.
     """
-    
+
     def __init__(self, arglist):
-        self.args = docopt(__doc__, arglist)    
-    
+        self.args = docopt(__doc__, arglist)
+
     def __str__(self):
         return str(self.args)
-    
+
     def __getattr__(self, key):
         return self.args[key]
 
-    @property 
+    @property
     def all_args(self):
-        return self.args 
-        
-    @property 
+        return self.args
+
+    @property
     def id(self):
         if self.args['<n>']:
-            return int(self.args['<n>'])        
-            
+            return int(self.args['<n>'])
+
     @property
     def task(self):
         subj = " ".join(self.args['<textlines>'])
-        return Task(subj)      
+        return Task(subj)
 
 
-def main(arglist=sys.argv[1:], file=FILENAME, out=sys.stdout):    
+def main(arglist=sys.argv[1:], file=FILENAME, out=sys.stdout):
     args = Arguments(arglist)
     tasklist = TaskList(file, out)
     #  guz.py new <textlines>...
@@ -231,60 +237,64 @@ def main(arglist=sys.argv[1:], file=FILENAME, out=sys.stdout):
         tasklist.add_item(args.task)
     #  guz.py list [<patterns>...]
     if args.list:
-        tasklist.list()       
-    #  guz.py del <n> 
+        tasklist.list()
+    #  guz.py del <n>
     if args.__getattr__('del'):
         tasklist.delete_item(args.id)
     #  guz.py <n> as <textlines>...
     if args.__getattr__('as'):
         tasklist.replace_item(args.id, args.task)
-    #  guz.py (rebase | delete) all   
+    #  guz.py (rebase | delete) all
     if args.all:
         if args.delete:
             tasklist.delete_all()
         elif args.rebase:
             tasklist.rebase()
-    return tasklist    
+    return tasklist
 
-#  guz.py <n> mark (done  | -d) 
-#  guz.py <n> mark (fail  | -f) 
-#  guz.py <n> mark (doubt | -?) 
-#  guz.py <n> mark (wait  | -w) [<input>] 
-#  guz.py <n> unmark 
+#  guz.py <n> mark (done  | -d)
+#  guz.py <n> mark (fail  | -f)
+#  guz.py <n> mark (doubt | -?)
+#  guz.py <n> mark (wait  | -w) [<input>]
+#  guz.py <n> unmark
 
-#  guz.py <n> due <datestamp> 
-#  guz.py <n> file <filename> 
-#  guz.py <n> [+<project>]... 
+#  guz.py <n> due <datestamp>
+#  guz.py <n> file <filename>
+#  guz.py <n> [+<project>]...
 #  guz.py <n> [@<context>]...
 #  guz.py datafile [<path>]
-#  guz.py [timer] start <n> 
+#  guz.py [timer] start <n>
 #  guz.py [timer] stop
 
+
 import io
+
+
 def catch_output(command_lines, file):
     """Intercept stdout stream when executing *command_lines* on *file*.
     """
     for command_line in command_lines.split('\n'):
-        arglist = command_line.split(' ')        
+        arglist = command_line.split(' ')
         out = io.StringIO()
         main(arglist, file, out)
-    return out.getvalue()   
+    return out.getvalue()
+
 
 def catch_tasklist(command_lines, file):
-    """Execute *command_lines* on *file* and return reulting tasklist."""    
+    """Execute *command_lines* on *file* and return reulting tasklist."""
     for command_line in command_lines.split('\n'):
         arglist = command_line.split(" ")
         result = main(arglist, file)
-    return result    
+    return result
 
-    
+
 if __name__ == '__main__':
-    
+
     c = catch_output('delete all', file="temp.pickle")
     assert c == 'All tasks deleted. What made you do this?..\n'
 
     c = catch_output('del 0', file="temp.pickle")
     assert c == 'No such task id: 0\nCannot delete task 0\n'
-    
+
     ta_li = catch_tasklist("delete all\nnew do this", file="temp.pickle")
     assert ta_li.tasks[1].subject == 'do this'
--- test_guz.py
+++ test_guz.py
@@ -2,12 +2,13 @@
 
 # -*- coding: utf-8 -*-
 
-# FIXME: play with delegator 
-#import delegator 
+# FIXME: play with delegator
+#import delegator
 #c = delegator.run("python -v")
-    
+
 import pytest
 import os
+import io
 
 
 from guz import DataStore, FILENAME
@@ -17,166 +18,154 @@ from guz import Arguments
 
 TEMP_FILENAME = "temp.pickle"
 
+
+def with_newline(x):
+    newline = '\n'
+    return '{}{}'.format(x, newline)
+
+
 def set_datastore(filename, contents):
     ds = DataStore(filename)
-    ds.to_disk(contents) 
+    ds.to_disk(contents)
 
-def teardown_module(module):
-    """Cleanup temp files."""
-    if os.path.exists(TEMP_FILENAME):
-        os.remove(TEMP_FILENAME)
-    
 
-class Test_DataStore:
-    
-    def setup_method(self): 
-        self._dict = {1:'task one', 2:'task two'}
-        set_datastore(TEMP_FILENAME, self._dict)
-        self.ds = DataStore(TEMP_FILENAME)
-            
-    def teardown_method(self):
-        """Cleanup temp files."""
-        if os.path.exists(TEMP_FILENAME):
-            os.remove(TEMP_FILENAME)
-    
-    def test_on_production_init_directs_to_global_filename(self):        
-        ds = DataStore()
-        assert ds.filename == FILENAME
-    
-    def test_on_temp_init_directs_to_other_filename(self):        
-        assert self.ds.filename == TEMP_FILENAME
+REF_DICT = {1: Task('do this'), 5: Task('do that')}
 
-    def test_on_temp_store_write_and_read_back_dictionary(self):        
-        assert self.ds.from_disk() == self._dict   
-
-import io
-
-class Test_Task:
-    
-    def setup_method(self):
-        self.t = Task("do this")
-        
-    def test_init_task_has_subject(self):
-        assert self.t.subject == "do this"
-        
-    def test_init_task_no_status_given(self):
-        assert self.t.status == None
-
-REF_DICT = {1:Task('do this'), 2:Task('do that')}
 
 def tasklist(filename=TEMP_FILENAME, tasks_dict=REF_DICT):
     set_datastore(filename, tasks_dict)
     out = io.StringIO()
     return TaskList(filename, out), out
 
-class Test_TaskList_No_Echo:
-    
+
+def teardown_module(module):
+    """Cleanup temp files."""
+    for filename in ("mock.pickle", TEMP_FILENAME):
+        if os.path.exists(filename):
+            os.remove(filename)
+
+
+class Test_DataStore:
+
+    def test_on_production_init_directs_to_global_filename(self):
+        ds = DataStore()
+        assert ds.filename == FILENAME
+
+
+class Test_DataStore_with_temp_file:
+
     def setup_method(self):
-        self.tasklist, _ = tasklist()         
-    
+        # some varialble to put and get from store
+        self.contents = 'abc', 0
+        set_datastore(TEMP_FILENAME, self.contents)
+        self.ds = DataStore(TEMP_FILENAME)
+
+    def test_on_temp_init_directs_to_other_filename(self):
+        assert self.ds.filename == TEMP_FILENAME
+
+    def test_on_temp_store_write_and_read_back_dictionary(self):
+        assert self.ds.from_disk() == self.contents
+
+
+class Test_Task:
+
+    def setup_method(self):
+        self.t = Task("do this")
+
+    def test_init_task_has_subject(self):
+        assert self.t.subject == "do this"
+
+    def test_init_task_no_status_given(self):
+        assert self.t.status is None
+
+
+class Test_TaskList:
+
+    def setup_method(self):
+        self.tasklist, self.out = tasklist()
+
     def test_tasks_attribute_is_dictionary(self):
-        self.tasklist.tasks == REF_DICT 
+        self.tasklist.tasks == REF_DICT
+
+    def test_ids_attribute_is_list(self):
+        assert isinstance(self.tasklist.ids, list)
+
+    def test_on_list(self):
+        self.tasklist.list()
+        assert self.out.getvalue() == """ 1 [ ] do this
+ 5 [ ] do that
+Listed 2 of 2 tasks
+"""
+
+    def test_on_delete_success(self):
+        self.tasklist.delete_item(1)
+        assert self.tasklist.tasks == {5: Task('do that')}
+        assert self.out.getvalue() == \
+            with_newline('Deleted task 1')
+        self.setup_method()
+
+    def test_on_delete_failed(self):
+        self.tasklist.delete_item(0)
+        assert self.out.getvalue() == (
+            with_newline('No such task id: 0') +
+            with_newline('Cannot delete task 0')
+        )
+
+    def test_on_rebase(self):
+        self.tasklist.rebase()
+        self.tasklist.tasks == {1: Task('do this'), 2: Task('do that')}
+        assert self.out.getvalue() == \
+            with_newline('Rebased task ids')
 
 
 class Test_Arguments:
-    
+
     def test_list(self):
         arglist = ['list']
         assert Arguments(arglist).list is True
-    
-    
+
+
+def empty_tasklist():
+    mockfile = "mock.pickle"
+    silent = io.StringIO()
+    mocklist = TaskList(mockfile, silent)
+    mocklist.delete_all()
+    return mocklist
+
+
+class Test_Behaviour_Testing:
+
+    def setup_method(self):
+        pass
+
+    def test_multiple_commands(self):
+        tasklist = empty_tasklist()
+        t = Task("one task @home - to delete")
+        tasklist.add_item(t)
+        t = Task("todo everything @home - to stay 1")
+        tasklist.add_item(t)
+        t = Task("other task @work - to stay 2")
+        tasklist.add_item(t)
+        # add and delete duplicate
+        t = Task("other task @work")
+        tasklist.add_item(t)
+        n = tasklist.get_max_id()
+        tasklist.delete_item(n)
+        t4 = Task("edited task 4 - to stay 3")
+        # FIXME: does not exit, not really replacing
+        tasklist.replace_item(4, t4)
+        tasklist.delete_item(1)
+
+        lines = [tasklist.tasks[i].subject for i in tasklist.select_all_ids()]
+        assert lines == ['todo everything @home - to stay 1',
+                         'other task @work - to stay 2',
+                         'edited task 4 - to stay 3']
+
+# TODO:
+#     lines = list(TaskList().select(['@work']))
+#     assert lines == ['3 other task @work - to stay 2']
+#
+
+
 if __name__ == "__main__":
-     pytest.main([__file__])
-     
-#     df = DataFile()
-#     assert isinstance(df.from_disk(), dict)
-#            
-#     # fixture with mock data file
-#     mockfile = "mock.pickle"
-#     mocklist = TaskList(mockfile)
-#     mocklist.delete_all() 
-#     
-#     #test 1
-#     tasklist =  TaskList(mockfile)
-#     assert isinstance(tasklist.ids, list)      
-#     
-#     #test 2
-#     t = Task("one task @home - to delete")
-#     tasklist.add_item(t) 
-#    
-#     t = Task("todo everything @home - to stay 1")
-#     tasklist.add_item(t)     
-#    
-#     t = Task("other task @work - to stay 2")
-#     tasklist.add_item(t) 
-#    
-#     # add and delete duplicate
-#     t = Task("other task @work")
-#     tasklist.add_item(t) 
-#     n = tasklist.get_max_id()
-#     tasklist.delete_item(n)
-#    
-#     t4 = Task("edited task 4 - to stay 3")
-#     tasklist.replace_item(4, t4)
-#    
-#     tasklist.delete_item(1)
-#   
-#     lines = list(TaskList().select_all())
-#     assert lines == ['2 todo everything @home - to stay 1',
-#                      '3 other task @work - to stay 2',
-#                      '4 edited task 4 - to stay 3']
-#
-#     lines = list(TaskList().select(['@work']))
-#     assert lines == ['3 other task @work - to stay 2']
-#
-#     #print something     
-#     TaskList(mockfile).print()
-#
-#
-#
-#class Test_DataStore:
-#    
-#     df = DataFile()
-#     assert isinstance(df.from_disk(), dict)
-#            
-#     # fixture with mock data file
-#     mockfile = "mock.pickle"
-#     mocklist = TaskList(mockfile)
-#     mocklist.delete_all() 
-#     
-#     #test 1
-#     tasklist =  TaskList(mockfile)
-#     assert isinstance(tasklist.ids, list)      
-#     
-#     #test 2
-#     t = Task("one task @home - to delete")
-#     tasklist.add_item(t) 
-#    
-#     t = Task("todo everything @home - to stay 1")
-#     tasklist.add_item(t)     
-#    
-#     t = Task("other task @work - to stay 2")
-#     tasklist.add_item(t) 
-#    
-#     # add and delete duplicate
-#     t = Task("other task @work")
-#     tasklist.add_item(t) 
-#     n = tasklist.get_max_id()
-#     tasklist.delete_item(n)
-#    
-#     t4 = Task("edited task 4 - to stay 3")
-#     tasklist.replace_item(4, t4)
-#    
-#     tasklist.delete_item(1)
-#   
-#     lines = list(TaskList().select_all())
-#     assert lines == ['2 todo everything @home - to stay 1',
-#                      '3 other task @work - to stay 2',
-#                      '4 edited task 4 - to stay 3']
-#
-#     lines = list(TaskList().select(['@work']))
-#     assert lines == ['3 other task @work - to stay 2']
-#
-#     #print something     
-#     TaskList(mockfile).print()
-#
+    pytest.main([__file__])
